8291c0284bcf13e80a11da2e296d39f5206adb35
Miguel Sales <miguelsales02@gmail.com>
2022-01-18 10:10:21 -0300

    Add home page and Navbar component

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+
+    <script></script>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
