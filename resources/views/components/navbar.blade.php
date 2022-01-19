8ae1e6a82a6f95a23da75438198412547361d09e
Miguel Sales <miguelsales02@gmail.com>
2022-01-19 09:57:48 -0300

    Changes navbar and creates Post component

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,19 +1,19 @@
-<nav class="navbar navbar-expand-lg navbar-custom navbar-dark">
+<nav class="navbar navbar-expand-lg navbar-custom">
     <div class="container-fluid">
-      <a class="navbar-brand " href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
       <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
+        <ul class="navbar-nav mx-auto">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
           </li>
          
         </ul>
---
+++ resources/views/components/post.blade.php
@@ -0,0 +1,20 @@
+<div class="post-card">
+    <div class="container-fluid">
+        <div class="img-container">
+            <img src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGhhY2t8ZW58MHx8MHx8&w=1000&q=80" alt="post image">
+        </div>
+
+        <div class="text-container">
+            <h1 class="post-title">MVC - Model View Controller</h1>
+            <p class="post-description">This a post about MVC, a design pattern used by many companies.</p>
+
+            <div class="read-more-container">
+                <a href="">
+                    Read More
+                    <i class="bi bi-arrow-bar-right"></i>
+                </a>
+            </div>
+        </div>
+       
+    </div>
+</div>
